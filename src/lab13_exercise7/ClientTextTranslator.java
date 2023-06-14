ab91269436c21a80e5bbc6932acdbe09eddfa73f
mustcoding <mahmormust@gmail.com>
2023-06-14 23:02:37 +0800

    lab13_exercise7

---
+++ src/lab13_exercise7/ClientTextTranslator.java
@@ -0,0 +1,60 @@
+package lab13_exercise7;
+
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+public class ClientTextTranslator {
+
+	public static void main(String[] args) {
+		
+		ClientFrame clientFrame = new ClientFrame();
+		String text = "Terima Kasih";
+		String translateTo = "English";
+		
+		try {
+			
+		// Launch client-side frame
+		
+		clientFrame.setVisible(true);
+			
+		// Connect to the server @ localhost, port 2345
+		Socket socket = new Socket(InetAddress.getLocalHost(), 2345);
+
+		// Update the status of the connection
+		clientFrame.updateConnectionStatus(socket.isConnected());
+		System.out.println(socket.isConnected());
+
+		// Write text for server
+		PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+		
+		// send text
+		printWriter.println(text);
+		printWriter.println(translateTo);
+		System.out.println("text send to server");
+		
+		// Read from network
+		BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+
+		System.out.println("test");
+		// display words count
+		String translateLanguage = bufferedReader.readLine();
+		clientFrame.updateWordsCount(translateLanguage);
+	
+
+		// Close everything
+		bufferedReader.close();
+		socket.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
